2f30f5f17bd77bd41ca8d655a9ba57d88deeb845
xYanchi <yanchitheone@gmail.com.com>
2020-09-27 05:22:22 -0400

    Added Jquery

--- static/script/script.js
+++ static/script/script.js
@@ -1,8 +1,5 @@
-// let email = document.getElementById("email");
 function darkMode() {
 	document.body.classList.toggle("dark-mode");
-	// document.getElementById("email").style.color = "white";
-
 	if (document.getElementById("mySwitch").checked) {
 		document.getElementById("email").style.color = "white";
 		document.getElementById("iconSun").style.filter = "invert(100%)";
@@ -13,3 +10,9 @@ function darkMode() {
 		document.getElementById("iconMoon").style.filter = "invert(0%)";
 	}
 }
+
+$(document).ready(function () {
+	darkMode();
+
+	$("#imgHandWave").hover()
+});
