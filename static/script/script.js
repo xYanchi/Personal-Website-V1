2951ca98ff0a3a125b4290ca12024571a0846965
xYanchi <yanchitheone@gmail.com.com>
2020-09-27 01:32:10 -0400

    Added group img

--- static/script/script.js
+++ static/script/script.js
@@ -1,4 +1,6 @@
 // let email = document.getElementById("email");
 function darkMode() {
   document.body.classList.toggle("dark-mode");
+  document.getElementById("email").style.color = "white";
+  document.getElementsByClassName("icon").style.color = "white";
 }
