2005e3be4455ce7c2a7980770733f4252246b807
xYanchi <yanchitheone@gmail.com.com>
2020-09-27 04:55:30 -0400

    Added Icons change white

--- static/script/script.js
+++ static/script/script.js
@@ -1,13 +1,15 @@
 // let email = document.getElementById("email");
 function darkMode() {
-  document.body.classList.toggle("dark-mode");
-  // document.getElementById("email").style.color = "white";
+	document.body.classList.toggle("dark-mode");
+	// document.getElementById("email").style.color = "white";
 
-  if (document.getElementById("mySwitch").checked) {
-    document.getElementById("email").style.color = "white";
-    // document.getElementsByClassName("icon").style.color = "white";
-  } else if (!document.getElementById("mySwitch").checked) {
-    document.getElementById("email").style.color = "black";
-    // document.getElementsByClassName("icon").style.color = "white";
-  }
+	if (document.getElementById("mySwitch").checked) {
+		document.getElementById("email").style.color = "white";
+		document.getElementById("iconSun").style.filter = "invert(100%)";
+		document.getElementById("iconMoon").style.filter = "invert(100%)";
+	} else if (!document.getElementById("mySwitch").checked) {
+		document.getElementById("email").style.color = "black";
+		document.getElementById("iconSun").style.filter = "invert(0%)";
+		document.getElementById("iconMoon").style.filter = "invert(0%)";
+	}
 }
