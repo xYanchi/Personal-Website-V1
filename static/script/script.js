c07b845278914e1ac240dc69da0631d1ed682abc
xYanchi <yanchitheone@gmail.com.com>
2020-09-27 02:38:22 -0400

    Added email darkmode

--- static/script/script.js
+++ static/script/script.js
@@ -1,6 +1,13 @@
 // let email = document.getElementById("email");
 function darkMode() {
   document.body.classList.toggle("dark-mode");
-  document.getElementById("email").style.color = "white";
-  document.getElementsByClassName("icon").style.color = "white";
+  // document.getElementById("email").style.color = "white";
+
+  if (document.getElementById("mySwitch").checked) {
+    document.getElementById("email").style.color = "white";
+    // document.getElementsByClassName("icon").style.color = "white";
+  } else if (!document.getElementById("mySwitch").checked) {
+    document.getElementById("email").style.color = "black";
+    // document.getElementsByClassName("icon").style.color = "white";
+  }
 }
