829439d311b97243f2495636c18825d473f2a591
xYanchi <haymondyantech@gmail.com>
2020-10-07 22:38:27 -0400

    Fix backgrond div

--- static/script/script.js
+++ static/script/script.js
@@ -7,21 +7,25 @@ function darkMode() {
 	let iconSun = document.getElementById("iconSun");
 	let iconMoon = document.getElementById("iconMoon");
 	let body = document.body;
+	let expCompany = document.getElementById("p-exp-company");
+	let school = document.getElementById("a-school");
 	if (document.getElementById("mySwitch").checked) {
 		email.style.color = "white";
+		expCompany.style.color = "white";
 		iconSun.style.filter = "invert(100%)";
 		iconMoon.style.filter = "invert(100%)";
-		body.style.backgroundColor = "rgb(1, 0, 13)";
+		body.style.backgroundColor = "rgb(1, 0, 20)";
 		body.style.color = "white";
+		school.style.color = "white";
 	} else if (!document.getElementById("mySwitch").checked) {
 		email.style.color = "black";
+		expCompany.style.color = "black";
 		iconSun.style.filter = "invert(0%)";
 		iconMoon.style.filter = "invert(0%)";
-		body.style.backgroundColor = "rgb(223, 223, 235)";
+		body.style.backgroundColor = "rgb(223, 223, 245)";
 		body.style.color = "black";
+		school.style.color = "black";
 	}
 }
-
 function emailBottomGoUp() {
-
 }
