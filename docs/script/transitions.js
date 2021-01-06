d9f862bb3dd7e11498410b7294353d1d7d159809
xYanchi <haymondyantech@gmail.com>
2021-01-06 16:03:19 -0500

    Starting point for my website, with all animations/transitions from techdstep repo

---
+++ docs/script/transitions.js
@@ -0,0 +1,56 @@
+new hoverEffect({
+    parent: document.querySelector('.img1'),
+    intensity: 0.1,
+    image1: './img/Foreground.jpg',
+    image2: './img/Background.jpg',
+    displacementImage: './img/heightMap.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.img2'),
+    intensity: 0.1,
+    image1: './img/Foreground.jpg',
+    image2: './img/Background.jpg',
+    displacementImage: './img/rect.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.img3'),
+    intensity: 0.9,
+    image1: './img/Foreground.jpg',
+    image2: './img/Background.jpg',
+    displacementImage: './img/stripe.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.img4'),
+    intensity: 0.1,
+    image1: './img/Foreground.jpg',
+    image2: './img/Background.jpg',
+    displacementImage: './img/stripe1.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.img5'),
+    intensity: 0.1,
+    image1: './img/Foreground.jpg',
+    image2: './img/Background.jpg',
+    displacementImage: './img/stripe1mul.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.img6'),
+    intensity: 0.1,
+    image1: './img/Foreground.jpg',
+    image2: './img/Background.jpg',
+    displacementImage: './img/submit.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.img7'),
+    intensity: 0.9,
+    image1: './img/Foreground.jpg',
+    image2: './img/Background.jpg',
+    displacementImage: './img/sun.png'
+});
+new hoverEffect({
+    parent: document.querySelector('.img8'),
+    intensity: 0.9,
+    image1: './img/Foreground.jpg',
+    image2: './img/Background.jpg',
+    displacementImage: './img/ocean.jpg'
+});
